56de22128b8d415a72f0a5831c003b7ea7dfb5fd
Пащенко Владимир <94864615+Pikimell@users.noreply.github.com>
2021-12-14 14:25:35 +0200

    Move params to repository

--- controllers/cardC.js
+++ controllers/cardC.js
@@ -1,5 +1,5 @@
 const {getCardsServ,getCardServ,pushCardServ,deleteCardServ,updateCardServ} = require('../services/cardS')
-const CARDS_TABLE = 'pashchenko-cards'
+
 
 const fetchDB = (callback) => {
     return (error, result) => {
@@ -25,12 +25,9 @@ const fetchDB = (callback) => {
 }
 
 const getCards = async (event, context, callback) => {
-    const params = {
-        TableName: CARDS_TABLE,
-    };
     const fetch = fetchDB(callback);
     try {
-        const res = await getCardsServ(params);
+        const res = await getCardsServ();
         return fetch(null, res);
     } catch (e) {
         return fetch(e, null);
@@ -38,15 +35,10 @@ const getCards = async (event, context, callback) => {
 };
 
 const getCard = async (event, context, callback) => {
-    const params = {
-        TableName: CARDS_TABLE,
-        Key: {
-            idCard: event.pathParameters.idCard,
-        },
-    };
+
     const fetch = fetchDB(callback);
     try {
-        const res = await getCardServ(params);
+        const res = await getCardServ(event);
         return fetch(null, res);
     } catch (e) {
         return fetch(e, null);
@@ -55,18 +47,9 @@ const getCard = async (event, context, callback) => {
 
 const pushCard = async (event, context, callback) => {
     const data = JSON.parse(event.body);
-    const params = {
-        TableName: CARDS_TABLE,
-        Item: {
-            idCard: data.idCard,
-            idColumn: data.idColumn,
-            title: data.title,
-            description: data.description
-        }
-    };
     const fetch = fetchDB(callback);
     try {
-        const res = await pushCardServ(params);
+        const res = await pushCardServ(data);
         return fetch(null, res);
     } catch (e) {
         return fetch(e, null);
@@ -74,15 +57,10 @@ const pushCard = async (event, context, callback) => {
 }
 
 const deleteCard = async (event, context, callback) => {
-    const params = {
-        TableName: CARDS_TABLE,
-        Key: {
-            idCard: event.pathParameters.idCard,
-        }
-    };
+
     const fetch = fetchDB(callback);
     try {
-        const res = await deleteCardServ(params);
+        const res = await deleteCardServ(event);
         return fetch(null, res);
     } catch (e) {
         return fetch(e, null);
@@ -91,23 +69,9 @@ const deleteCard = async (event, context, callback) => {
 
 const updateCard = async (event, context, callback) => {
     const data = JSON.parse(event.body);
-    const params = {
-        TableName: CARDS_TABLE,
-        Key: {
-            idCard: event.pathParameters.idCard,
-        },
-        UpdateExpression:
-            "set title = :title, description = :description",
-        ExpressionAttributeValues: {
-            ":title": data.title,
-            ":description": data.description
-        },
-
-        ReturnValues: "ALL_NEW"
-    };
     const fetch = fetchDB(callback);
     try {
-        const res = await updateCardServ(params);
+        const res = await updateCardServ({event:event, data:data});
         return fetch(null, res);
     } catch (e) {
         return fetch(e, null);
--- repositories/columnsR.js
+++ repositories/columnsR.js
@@ -1,22 +1,57 @@
 const {dynamoDb} = require("./connection");
+const COLUMNS_TABLE = 'pashchenko-columns'
 
-const getColumns = (params) => {
+const getColumns = () => {
+    const params = {
+        TableName: COLUMNS_TABLE,
+    };
     return dynamoDb.scan(params).promise();
 }
 
-const getColumn = (params) => {
+const getColumn = (event) => {
+    const params = {
+        TableName: COLUMNS_TABLE,
+        Key: {
+            idColumn: event.pathParameters.idColumn,
+        },
+    }
     return dynamoDb.get(params).promise();
 }
 
-const pushColumn = (params) => {
+const pushColumn = (data) => {
+    const params = {
+        TableName: COLUMNS_TABLE,
+        Item: {
+            idColumn: data.idColumn,
+            title: data.title,
+        }
+    };
     return dynamoDb.put(params).promise();
 }
 
-const deleteColumn = (params) => {
+const deleteColumn = (event) => {
+    const params = {
+        TableName: COLUMNS_TABLE,
+        Key: {
+            idColumn: event.pathParameters.idColumn,
+        }
+    };
     return dynamoDb.delete(params).promise();
 }
 
-const updateColumn = (params) => {
+const updateColumn = ({event, data}) => {
+    const params = {
+        TableName: COLUMNS_TABLE,
+        Key: {
+            idColumn: event.pathParameters.idColumn,
+        },
+        UpdateExpression:
+            "set title = :title",
+        ExpressionAttributeValues: {
+            ":title": data.title
+        },
+        ReturnValues: "ALL_NEW"
+    };
     return dynamoDb.update(params).promise();
 }
 
