4135f3cb72df069157851bfa23c97bd0d079a434
unknown <pikimelposhta@gmail.com>
2022-01-04 02:50:39 +0200

    cognito 1

--- controllers/authorizationC.js
+++ controllers/authorizationC.js
@@ -1,60 +1,67 @@
-import 'cross-fetch/polyfill';
-import AmazonCognitoIdentity from 'amazon-cognito-identity-js';
-import * as AWS from 'aws-sdk/global';
-
-const CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
-
 const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+const jwt_decode = require('jwt-decode');
 
-
-
-const authenticationData = {
-    Username: 'username',
-    Password: 'password',
-};
-const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(
-    authenticationData
-);
 const poolData = {
-    UserPoolId: '...', // Your user pool id here
-    ClientId: '...', // Your client id here
+    UserPoolId: 'us-east-2_dtSlpb6U3',
+    ClientId: '5ptet6oq1th1i3cpdjaf7no20f'
 };
 const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-const userData = {
-    Username: 'username',
-    Pool: userPool,
-};
-const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
-cognitoUser.authenticateUser(authenticationDetails, {
-    onSuccess: function(result) {
-        const accessToken = result.getAccessToken().getJwtToken();
 
-        //POTENTIAL: Region needs to be set if not already set previously elsewhere.
-        AWS.config.region = '<region>';
 
-        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-            IdentityPoolId: '...', // your identity pool id here
-            Logins: {
-                // Change the key below according to the specific region your user pool is in.
-                'cognito-idp.<region>.amazonaws.com/<YOUR_USER_POOL_ID>': result
-                    .getIdToken()
-                    .getJwtToken(),
+function getCognitoUser(login){
+    let userData = {
+        Username: login,
+        Pool: userPool
+    };
+
+    return new AmazonCognitoIdentity.CognitoUser(userData);
+}
+
+function getAuthDetails(login,password){
+    let userData = {
+        Username: login,
+        Password: password
+    };
+
+    return new AmazonCognitoIdentity.AuthenticationDetails(userData);
+}
+
+
+function getJWT(token){
+    let {email,exp,auth_time,token_use,sub} = jwt_decode(token.idToken);
+    return {token, email, exp, auth_time, token_use, sub};
+}
+
+const signIn = async (event) => {
+    const data = JSON.parse(event.body);
+    const {email,password} = data;
+
+    return new Promise((resolve) => {
+        getCognitoUser(email).authenticateUser(getAuthDetails(email,password),{
+
+            onSuccess: (result) => {
+                const token = {
+                    accessToken: result.getAccessToken(),
+                    idToken: result.getIdToken(),
+                    refreshToken: result.getRefreshToken()
+                };
+
+                resolve({
+                    statusCode: 200,
+                    headers: {"Access-Control-Allow-Origin":"*"},
+                    body: JSON.stringify(getJWT(token))
+                });
             },
-        });
-
-        //refreshes credentials using AWS.CognitoIdentity.getCredentialsForIdentity()
-        AWS.config.credentials.refresh(error => {
-            if (error) {
-                console.error(error);
-            } else {
-                // Instantiate aws sdk service objects now that the credentials have been updated.
-                // example: const s3 = new AWS.S3();
-                console.log('Successfully logged!');
+            onFailure: function() {
+                resolve({
+                    statusCode: 400,
+                    body: JSON.stringify(getAuthDetails(email,password))
+                });
             }
         });
-    },
+    });
+};
 
-    onFailure: function(err) {
-        alert(err.message || JSON.stringify(err));
-    },
-});
+export {
+    signIn
+};
