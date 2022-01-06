e80a4a40be07c67789b0aa71db60b1f3043b6ef4
Пащенко Владимир <94864615+Pikimell@users.noreply.github.com>
2022-01-06 15:27:34 +0200

    Refresh and SignOut

--- controllers/authorization/refreshToken.js
+++ controllers/authorization/refreshToken.js
@@ -1,34 +1,48 @@
-import {CognitoRefreshToken} from "amazon-cognito-identity-js";
+import {CognitoRefreshToken, CognitoAccessToken, CognitoIdToken, CognitoUserSession} from "amazon-cognito-identity-js";
+
 const {getCognitoUser} = require('./userConst');
 
-const refreshToken = function (event) {
+
+const getSessionsData = (tokens) => {
+    const AccessToken = new CognitoAccessToken({AccessToken: tokens.accessToken.jwtToken});
+    const IdToken = new CognitoIdToken({IdToken: tokens.idToken.jwtToken});
+    const RefreshToken = new CognitoRefreshToken({RefreshToken: tokens.refreshToken.token});
+
+    const sessionData = {
+        IdToken: IdToken,
+        AccessToken: AccessToken,
+        RefreshToken: RefreshToken
+    };
+
+    return new CognitoUserSession(sessionData);
+};
+
+const refreshToken = (event) => {
     const data = JSON.parse(event.body);
     const {username, tokens} = data;
-    const RefreshToken = new CognitoRefreshToken({ RefreshToken: tokens.refreshToken });
+    const token = new CognitoRefreshToken({RefreshToken: tokens.refreshToken});
     let cognitoUser = getCognitoUser(username);
+    cognitoUser.setSignInUserSession(getSessionsData(tokens));
 
-    try {
-        cognitoUser.refreshSession(RefreshToken, (err, session) => {
-            return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
+        cognitoUser.getSession(function (err, session) {
+            if(!session.isValid()){
                 resolve({
-                    statusCode: 201,
-                    headers: {"Access-Control-Allow-Origin": "*"},
+                    statusCode: 200,
+                    headers: {"Content-Type": "json/plain"},
                     body: JSON.stringify(session)
                 });
-            });
+            }else{
+                cognitoUser.refreshSession(token, (err, session) => {
+                    resolve({
+                        statusCode: 200,
+                        headers: {"Content-Type": "json/plain"},
+                        body: JSON.stringify(session)
+                    });
+                });
+            }
         });
-    } catch (err) {
-        return new Promise((resolve) => {
-            resolve({
-                statusCode: 400,
-                headers: {"Access-Control-Allow-Origin": "*"},
-                body: err.toString()
-                //body:JSON.stringify(tokens)
-            });
-        });
-    }
-
-
+    });
 };
 
 
---
+++ controllers/authorization/signOut.js
@@ -0,0 +1,16 @@
+const {getCognitoUser,getAuthDetails} = require('./userConst');
+
+const signOut = (event) => {
+    const data = JSON.parse(event.body);
+    const {username} = data;
+    let cognitoUser = getCognitoUser(username);
+    return new Promise((resolve) => {
+        cognitoUser.signOut(()=>{
+            resolve({body:'signOut Ok!'})
+        })
+    });
+}
+
+export {
+    signOut
+}
