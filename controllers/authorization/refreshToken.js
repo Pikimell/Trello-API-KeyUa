f4149897f6de3ddec90691baf5fdde5242c0a8e6
Пащенко Владимир <94864615+Pikimell@users.noreply.github.com>
2022-01-14 18:12:25 +0200

    test

--- controllers/authorization/refreshToken.js
+++ controllers/authorization/refreshToken.js
@@ -9,22 +9,34 @@ const refreshToken = (event) => {
     const token = new CognitoRefreshToken({RefreshToken: refreshToken});
     let cognitoUser = getCognitoUser(username);
 
-    console.log("\n\n\n-------------------");
-    console.log(data);
-    console.log("-------------------\n\n\n");
-
     return new Promise((resolve) => {
         cognitoUser.refreshSession(token, (err, session) => {
             if (err) {
                 resolve({
                     statusCode: 400,
-                    headers: {"Access-Control-Allow-Origin": "*"},
+                    headers: {
+                        "Access-Control-Allow-Origin": "*",
+                        'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+                        'Access-Control-Allow-Credentials': true
+                    },
                     body: err.toString()
                 });
             } else {
+
+                const tokens = {
+                    accessToken: session.getAccessToken().getJwtToken(),
+                    idToken: session.getIdToken(),
+                    refreshToken: session.getRefreshToken().getToken()
+                };
+                cognitoUser['tokens'] = tokens;
+
                 resolve({
                     statusCode: 200,
-                    headers: {"Access-Control-Allow-Origin": "*"},
+                    headers: {
+                        "Access-Control-Allow-Origin": "*",
+                        'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+                        'Access-Control-Allow-Credentials': true
+                    },
                     body: JSON.stringify(session)
                 });
             }
